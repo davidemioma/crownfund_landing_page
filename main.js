8b3c3cc3ae8146361d0170a481a6e96f62129d83
davidemioma <davidemiomauche@gmail.com>
2021-11-30 13:32:06 +0100

    update page

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible_expanded");
-  })
-);
+//Selectors
+const navToggler = document.querySelector(".nav_toggler");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close-modal");
+const openRewardModal = document.querySelectorAll(".open_reward-modal");
+const rewardModal = document.querySelector(".reward_modal");
+
+//Function
+const openModal = function () {
+  modal.style.display = "block";
+};
+
+const closeModal = function () {
+  modal.style.display = "none";
+};
+
+const openRewardModal = function () {
+  rewardModal.classList.remove("hidden");
+};
+
+//Event Listeners
+navToggler.addEventListener("click", openModal);
+
+closeBtn.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+});
+
+openRewardModal.forEach((btn) => {
+  btn.addEventListener("click", openRewardModal);
+});
