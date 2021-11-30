92fc7d07d72757adad3765a83f9d87d1b171002c
davidemioma <davidemiomauche@gmail.com>
2021-11-30 14:46:07 +0100

    page update

--- main.js
+++ main.js
@@ -2,8 +2,14 @@
 const navToggler = document.querySelector(".nav_toggler");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close-modal");
-const openRewardModal = document.querySelectorAll(".open_reward-modal");
+const openRewardModalBtn = document.querySelectorAll(".open_reward-modal");
 const rewardModal = document.querySelector(".reward_modal");
+const closeRewardModalBtn = document.querySelector(".close_reward-modal");
+const noReward = document.querySelector(".no-reward");
+const bambooReward = document.querySelector(".bamboo");
+const BlackEditionReward = document.querySelector(".black_editon");
+const submitted = document.querySelector(".submit_modal");
+const closeSubmitModalBtn = document.querySelector(".submit_modal button");
 
 //Function
 const openModal = function () {
@@ -18,17 +24,63 @@ const openRewardModal = function () {
   rewardModal.classList.remove("hidden");
 };
 
+const closeRewardModal = function () {
+  rewardModal.classList.add("hidden");
+};
+
+const activateReward = function (reward) {
+  reward.querySelector(".circle span").classList.remove("hidden");
+
+  reward.querySelector(".details").classList.remove("hidden");
+
+  reward.querySelector("button").addEventListener("click", function (e) {
+    const input = +reward.querySelector("input").value;
+
+    if (input === "" || input < 1) return;
+
+    submitted.style.display = "grid";
+
+    rewardModal.classList.add("hidden");
+  });
+};
+
+const deactivateReward = function (reward) {
+  reward.querySelector(".circle span").classList.add("hidden");
+
+  reward.querySelector(".details").classList.add("hidden");
+};
+
+const closeSubmitModal = function () {
+  submitted.style.display = "none";
+};
+
 //Event Listeners
 navToggler.addEventListener("click", openModal);
 
 closeBtn.addEventListener("click", closeModal);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    closeModal();
-  }
-});
-
-openRewardModal.forEach((btn) => {
+openRewardModalBtn.forEach((btn) => {
   btn.addEventListener("click", openRewardModal);
 });
+
+closeRewardModalBtn.addEventListener("click", closeRewardModal);
+
+noReward.addEventListener("click", function () {
+  activateReward(noReward);
+  deactivateReward(bambooReward);
+  deactivateReward(BlackEditionReward);
+});
+
+bambooReward.addEventListener("click", function () {
+  activateReward(bambooReward);
+  deactivateReward(noReward);
+  deactivateReward(BlackEditionReward);
+});
+
+BlackEditionReward.addEventListener("click", function () {
+  activateReward(BlackEditionReward);
+  deactivateReward(noReward);
+  deactivateReward(bambooReward);
+});
+
+closeSubmitModalBtn.addEventListener("click", closeSubmitModal);
